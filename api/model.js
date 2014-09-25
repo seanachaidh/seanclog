43d18d53d341e322369dd40663d2284076e17c61
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2014-09-25 16:58:29 +0200

    datamodellen maken

---
+++ api/model.js
@@ -0,0 +1,34 @@
+/**
+ * De modellen voor onze applicatie
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	gebruikersnaam: String,
+	wachtwoord: String,
+	naam: String,
+	email: String
+});
+
+var projectSchema = new Schema({
+	titel: String,
+	prijs: Number,
+	gebruiker: {type: Schema.types.ObjectId, ref: 'Gebruiker'}
+});
+
+var klantSchema = new Schema ({
+	naam: String,
+	telefoonnummer: String,
+	email: String,
+	gebruiker: {type: Schema.types.ObjectId, ref: 'Gebruiker'}
+});
+
+var trackSchema = new Schema({
+	titel: String,
+	begintijd: Date,
+	eindtijd: Date,
+	project: {type: Schema.types.ObjectId, ref: 'Project'},
+	gebruiker: {type: Schema.types.ObjectId, ref: 'Gebruiker'}
+});
