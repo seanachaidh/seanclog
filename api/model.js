33ad2fd7d10246d2c09e643a29129bdedc443a49
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2014-10-06 16:44:23 +0200

    modelreferenties werken

--- api/model.js
+++ api/model.js
@@ -14,20 +14,20 @@ var userSchema = new Schema({
 	wachtwoord: String,
 	naam: String,
 	email: String
-});
+}, {collection: "gebruikers"});
 
 var projectSchema = new Schema({
 	titel: String,
 	prijs: Number,
 	gebruiker: {type: Schema.Types.ObjectId, ref: 'Gebruiker'}
-});
+}, {collection: "projecten"});
 
 var klantSchema = new Schema ({
 	naam: String,
 	telefoonnummer: String,
 	email: String,
 	gebruiker: {type: Schema.Types.ObjectId, ref: 'Gebruiker'}
-});
+}, {collection: "klanten"});
 
 var trackSchema = new Schema({
 	titel: String,
@@ -35,7 +35,7 @@ var trackSchema = new Schema({
 	eindtijd: Date,
 	project: {type: Schema.Types.ObjectId, ref: 'Project'},
 	gebruiker: {type: Schema.Types.ObjectId, ref: 'Gebruiker'}
-});
+}, {collection: "tracks"});
 
 exports.Track = mongoose.model('Track', trackSchema);
 exports.Project = mongoose.model('Project', projectSchema);
--- api/projects.js
+++ api/projects.js
@@ -6,9 +6,7 @@ var ObjectId = require('mongoose').Types.ObjectId;
 
 exports.getProjectsOfUser = function(req, res) {
 	var id = req.param('userid');
-	model.Gebruiker.findById(id, '', {lean:true}, function(err, docs) {
-		model.Project.find({gebruiker: docs._id}, function(err, projs){
-			res.json(projs);
-		});
+	model.Project.find({gebruiker: new ObjectId(id)}, function(err, docs) {
+		res.json(docs);
 	});
 };
