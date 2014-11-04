9a8dda38512a5662a71783435ac897b24fa37a8c
seanachaidh <pvankeymeulen@seanachaidh.be>
2014-11-04 13:38:06 +0100

    Ma travail du matin. Il y a quelques choses qui ne marchent pas.

--- public/javascripts/ang-controllers.js
+++ public/javascripts/ang-controllers.js
@@ -4,27 +4,47 @@
  */
 var seanControllers = angular.module('seanClogControllers',['seanClogServices', 'ngRoute']);
 
-seanControllers.controller('ProjectController', ['$scope', '$routeParams', 'Projects',
-function($scope, $routeParams, Projects){
+seanControllers.controller('ProjectController', ['$scope', '$route', 'Projects',
+function($scope, $route, Projects){
+	//Dans cette function, on utilise le nom "proj" trop beaucoup pour un variable
+	
 	$scope.tableTitle = "Mijn Projecten";
 	Projects.query(function(proj){
 		$scope.myData = proj;
 		$scope.createForm = '/partials/forms/form_createproject.html';
 		$scope.createProj = function(proj) {
-			//estas tiu funkcio necesa?
+			//Is deze functie nodig
 			var tmp = angular.copy(proj);
-			//post la kreo de la projecta. fermu la fenestra
 			Projects.post(proj);
+			angular.element("#createModal").modal("hide");
+			
+			//Recharge la page
+			//je n'ai pas déjà testé cette function.
+			$route.reload();
 		}
 	});
 }]);
 
 seanControllers.controller('TracksController',
-		['$scope', '$routeParams', 'Tracks', function($scope, $routeParams, Tracks){
+		['$scope', '$routeParams', 'Tracks', 'Projects',
+		 function($scope, $routeParams, Tracks, Projects){
 			$scope.tableTitle = "Mijn Tracks";
+			//choisit la dialogue pour faire des tracks
+			$scope.createForm = '/partials/forms/form_createtrack.html';
+			
 			Tracks.query(function(t){
 				$scope.myData = t;
-			}); 
+			});
+			/*
+			 * Nous avons besoin de tous les projects de l' utilisateur
+			 * Ansi l' utilisateur peut choisisez un project 
+			 */
+			Projects.query(function(proj){
+				$scope.pojects = proj;
+			});
+			
+			
+			
 		}]);
 
 seanControllers.controller('TestController', ['$scope', 'Tests', function($scope, Tests) {
