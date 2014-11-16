e5c60efd8ae9622b6421bc9a5e893758d8199e24
seanachaidh <pvankeymeulen@seanachaidh.be>
2014-11-16 11:54:05 +0100

    de pdf api vervoledigen

--- api/pdfexport.js
+++ api/pdfexport.js
@@ -7,15 +7,52 @@
  */
 
 var phantom = require('phantom');
+var model = require('./model');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 exports.getClients = function(req, res) {
-	//Hier worden pdf's gemaakt
+	//We gaan niets verzenden als er geen gebruiker is aangemeld
+	if(!req.user){
+		res.json({value: false});
+	} else {
+		//Deze variabele gaan we gebruiken in onze queries.
+		var g = new ObjectId(req.user._id);
+		model.Klant.find({gebruiker: g}, function(err, f) {
+			if(err) {
+				res.send({value: false});
+			} else {
+				res.json(f);
+			}
+		});
+	}
 };
 
 exports.getProjects = function(req, res) {
-	
+	if(!req.user) {
+		res.json({value: false});
+	} else {
+		var g = new ObjectId(req.user._id);
+		model.Project.find({gebruiker: g}, function(err, f){
+			if(err) {
+				res.json({value: false});
+			} else {
+				res.json(f);
+			}
+		});
+	}
 };
 
 exports.getTracks = function(req, res) {
-	
+	if(!req.user) {
+		res.json({value: false});
+	} else {
+		var g = new ObjectId(req.user._id);
+		model.Track.find({gebruiker: g}, function(err, f) {
+			if(err) {
+				res.json({value: false});
+			} else {
+				res.json(f);
+			}
+		});
+	}
 };
