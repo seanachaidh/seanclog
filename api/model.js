ce90925fb8eedc02619364ca5eac110355d66e56
seanachaidh <pvankeymeulen@seanachaidh.be>
2014-11-24 15:16:49 +0100

    Uitgedokterd hoe ik het programma ga parsen.

--- api/model.js
+++ api/model.js
@@ -4,6 +4,44 @@
 
 var mongoose = require('mongoose');
 
+//de text die op dit moment geparsed is
+var ctext = '';
+var fs = require('fs');
+var parser = require('sax').parser(true);
+
+parser.onerror = function(e) {
+	console.log('er was een fout in het xmlbestand');
+};
+
+parser.ontext = function(t) {
+	if(ctext == "password") {
+		
+	}
+	
+	if(ctext == "username") {
+		
+	}
+	
+	if(ctext == "host") {
+		
+	}
+};
+
+parser.onopentag = function(node) {
+	ctext = node.name;
+};
+
+fs.readFile('../passconf.xml', 'utf8', function(err, data) {
+	if(err) {
+		console.log('er is geen paswoord vastgelegd');
+	} else {
+		//deze code is slechts tijdelijk om mijn app makkelijker te kunnen debuggen
+		console.log('pasconf gevonden');
+		console.log(data);
+	}
+});
+
+
 //connecteren met de databank
 mongoose.connect('mongodb://pietervk:ragnarok@lennon.mongohq.com:10094/app29880742');
 
