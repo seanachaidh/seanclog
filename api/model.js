5fa9b8d0be0596b4e4fd41b2a38325053f9cec88
seanachaidh <pvankeymeulen@seanachaidh.be>
2014-12-13 15:27:52 +0100

    Xml uit de code verwijderen

--- api/model.js
+++ api/model.js
@@ -7,7 +7,6 @@ var mongoose = require('mongoose');
 //de text die op dit moment geparsed is
 var ctext = '';
 var fs = require('fs');
-var parser = require('sax').parser(true);
 
 //gegevens om te connecteren
 var pass = '',
@@ -15,41 +14,6 @@ var pass = '',
 	user = '',
 	db = '';
 	connectstr = '';
-
-parser.onerror = function(e) {
-	console.log('er was een fout in het xmlbestand');
-};
-
-parser.ontext = function(t) {
-	//TODO: De string t moet getrimed worden
-	if(ctext == "password") {
-		pass = t;
-		ctext = '';
-	}
-	
-	if(ctext == "username") {
-		user = t;
-		ctext = '';
-	}
-	
-	if(ctext == "host") {
-		host = t;
-		ctext = '';
-	}
-};
-
-parser.onend = function() {
-	console.log('parser voltooid');
-	console.log('user: ' + user);
-	console.log('password: '+ pass);
-	console.log('host: ' + host);
-	
-
-};
-
-parser.onopentag = function(node) {
-	ctext = node.name;
-};
 
 fs.readFile('passconf.json', 'utf8', function(err, data) {
 	if(err) {
