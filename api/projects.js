3e67f006dae9baa336bcb80943218edd6c119a6e
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2015-01-11 14:39:11 +0100

    Het opvragen van projecten werkt weer

--- api/projects.js
+++ api/projects.js
@@ -8,18 +8,15 @@ var ObjectId = require('mongoose').Types.ObjectId;
  * Krijg alle projecten van de huidig aangemelde gebruiker
  */
 exports.getProjectsOfUser = function(req, res) {
-	if(!req.user) {
-		res.json([{value:false}]);
-	} else {
-		// vergeet de api en gebruik hier direct de gewone sessie
-		var id = req.user._id;
-		model.Project.find({
-			gebruiker : new ObjectId(id)
-		}, function(err, docs) {
-			res.json(docs);
+	var token = req.param('access_token');
+	
+	model.Gebruiker.findOne({token: token}, function(err, doc) {
+		var id = doc._id;
+		
+		model.Project.find({gebruiker: id}, function(err, project){
+			res.json(project);
 		});
-	}
-
+	});
 };
 
 exports.pdfProject = function(req, res) {
